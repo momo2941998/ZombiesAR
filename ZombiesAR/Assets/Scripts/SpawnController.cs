a3010e37dc54999442e18c0a58060a983f71400f
thanh.nq <momo2941998@gmail.com>
2020-05-12 10:30:18 +0700

    cho nấm mồ vào

---
+++ ZombiesAR/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject FemaleZombiePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnZombie", 2, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnZombie()
+    {
+        Vector3 position = gameObject.transform.position;
+
+        GameObject zombieGO = Instantiate(FemaleZombiePrefab, new Vector3(position.x, position.y, position.z), Quaternion.Euler(0, Random.Range(0f, 360f), 0));
+    }
+
+}
