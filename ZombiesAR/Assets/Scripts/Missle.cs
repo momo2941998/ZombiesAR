877e3fc8f7c4b050157578a10d762572a716875d
thanh.nq <momo2941998@gmail.com>
2020-05-31 08:14:32 +0700

    thêm skill phóng tên lửa

---
+++ ZombiesAR/Assets/Scripts/Missle.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Missle : MonoBehaviour
+{
+    public float damage;
+    private ParticleSystem boom;
+    private AudioSource audio;
+    public AudioClip launchSound;
+    public AudioClip explosionSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boom = GetComponent<ParticleSystem>();
+        audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(launchSound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Enemy")|| collision.gameObject.CompareTag("Ground"))
+        {
+            Explosion();
+            
+            //collision.gameObject.GetComponent<ZombieController>().TakeDamage(damage);
+        }
+
+    }
+
+    private void Explosion()
+    {
+
+        boom.Play();
+        audio.PlayOneShot(explosionSound);
+        Collider[] overlap = Physics.OverlapSphere(transform.position, 10);
+        foreach (Collider x in overlap)
+        {
+            if (x.gameObject.GetComponent<ZombieController>() != null)
+            {
+                x.gameObject.GetComponent<ZombieController>().TakeDamage(damage);
+                x.gameObject.GetComponent<Rigidbody>().AddExplosionForce(0, transform.position , 10);
+            }
+        }
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        gameObject.layer = 2;
+        Destroy(gameObject, 5);
+    }
+}
---
+++ ZombiesAR/Assets/Scripts/MissleButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissleButton : MonoBehaviour
+{
+    public GameObject misslePrefab;
+    private Image percentImage;
+    public float timeToFill;
+    public float lastTimeFill;
+    public Camera mainCam;
+    public float pushForce;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastTimeFill = timeToFill;
+        percentImage = GetComponent<Image>();
+        UpdateFillAmount();
+        GetComponent<Button>().onClick.AddListener(LauchMissle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RefillTime(Time.deltaTime);
+    }
+
+    private void RefillTime(float timeAdd)
+    {
+        float x = lastTimeFill + timeAdd;
+        if (x > timeToFill) lastTimeFill = timeToFill;
+        else
+        {
+            lastTimeFill = x;
+        }
+        UpdateFillAmount();
+    }
+
+    private void UpdateFillAmount()
+    {
+        percentImage.fillAmount = lastTimeFill / timeToFill;
+    }
+
+    public void LauchMissle()
+    {
+        if (lastTimeFill == timeToFill)
+        {
+            GameObject missle = Instantiate(misslePrefab, mainCam.transform.position, mainCam.transform.rotation) as GameObject;
+            missle.GetComponent<Rigidbody>().AddForce(pushForce * Vector3.forward, ForceMode.Impulse);
+            
+            ResetMissle();
+        }
+
+    }
+
+    private void ResetMissle()
+    {
+        lastTimeFill = 0;
+        UpdateFillAmount();
+    }
+}
